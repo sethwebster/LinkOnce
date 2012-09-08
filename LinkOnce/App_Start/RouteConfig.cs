e332a8bb46c351aa9e5894097c8dd4b85cc2046c
Seth Webster <sethwebster@gmail.com>
2012-09-08 13:24:12 -0700

    Changed Default Route

--- LinkOnce/App_Start/RouteConfig.cs
+++ LinkOnce/App_Start/RouteConfig.cs
@@ -22,7 +22,7 @@ namespace LinkOnce
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Links", action = "Create", id = UrlParameter.Optional }
             );
 
             
