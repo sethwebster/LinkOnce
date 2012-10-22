73a65e2b32a34af2ee70f6edeae4a09b16183465
sethwebster <sethwebster@gmail.com>
2012-10-21 22:06:57 -0700

    Added signalR and realtime updates

---
+++ LinkOnce/Hubs/LinkStatusHub.cs
@@ -0,0 +1,45 @@
+﻿using LinkOnce.Models;
+using SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LinkOnce.Hubs
+{
+    public class LinkStatusHub : Hub
+    {
+        IDatabaseContext context;
+        public LinkStatusHub()
+            : this(new DatabaseContext())
+        {
+        }
+        public LinkStatusHub(IDatabaseContext context)
+        {
+            this.context = context;
+        }
+
+        public void GetLinkStatus(int LinkId)
+        {
+            var link = context.Links.FirstOrDefault(l => l.LinkId == LinkId);
+            if (link != null)
+            {
+                if (link.IsUsed)
+                {
+                    Caller.updateStatus(new { Status = true, DateRetrieved = link.DateUsed, 
+                        Message = "Retrieved " + link.DateUsed.ToLongDateString() + " " + link.DateUsed.ToLongTimeString() });
+                }
+                else
+                {
+                    Caller.updateStatus(new { Status = true, Message = "Not retrieved" });
+                    Caller.scheduleRecheck();
+                }
+            }
+            else
+            {
+                Caller.updateStatus(new { Status = false, Message = "Link not found" });
+                Caller.scheduleRecheck();
+            }
+        }
+    }
+}
